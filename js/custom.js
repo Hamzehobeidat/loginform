a1ab9caeb4cab7ae8ce1f075f92d32c8ba481da6
osamaaladamat <osama.ala3damat@gmail.com>
2020-10-20 12:39:30 +0300

    loginform

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+var fname = document.getElementById("firstname");
+var lname = document.getElementById("lastname");
+var email = document.getElementById("email");
+var phone = document.getElementById("phone");
+var password = document.getElementById("password");
+var reppassword = document.getElementById("reppassword");
+
+
+
+
+
+
+
+/* fname.onfocus = function() {
+    document.getElementById("alertsuccess").style.display = "block";
+  }
+
+  fname.onblur = function() {
+    document.getElementById("alertsuccess").style.display = "none";
+  }
+
+    fname.onkeyup = function() {
+        // Validate lowercase letters
+        var lowerCaseLetters = /[A-Za-z]/g;
+        if(myInput.value.match(lowerCaseLetters)) {  
+          document.getElementById('alertsuccess').innerHTML = (' <div class="alert alert-success" role="alert"> A simple success alert—check it out! </div> ')
+        } else {
+            document.getElementById('alertsuccess').innerHTML = (' <div class="alert alert-danger" role="alert"> A simple danger alert—check it out! </div> ')
+        }
+ 
+    }
+ */
